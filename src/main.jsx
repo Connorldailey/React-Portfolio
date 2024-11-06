55f769447b78114e064d33a0e006d01f76d69ef3
Connor Dailey <connorldailey@gmail.com>
2024-11-06 10:11:52 -0500

    Set up react router in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// Bring in pages the router will use
+import App from './App';
+
+// Define the accessible routes
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 )
