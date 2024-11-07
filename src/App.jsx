3c88321cb3c149281433dd34f7971e92a761da51
Connor Dailey <connorldailey@gmail.com>
2024-11-07 11:10:04 -0500

    Establish footer layout and content

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <Header />
-      <main className="px-5 py-4">
+      <main className="px-5 py-4 flex-grow-1">
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,32 @@
 export default function Footer() {
+
+    const handleGitHubClick = () => {
+        window.open("https://github.com/connorldailey" , "_blank");
+    };
+
+    const handleLinkedInClick = () => {
+        window.open("https://www.linkedin.com/in/connor-dailey-7a129b2b2/", "_blank");
+    };
+
+    const handleInstagramClick = () => {
+        window.open("https://www.instagram.com/connorldailey", "_blank");
+    };
+
     return (
-        <footer>
-            <h2>Footer</h2>
+        <footer className="bg-dark p-3">
+            <div className="container-fluid">
+                <div className="d-flex justify-content-center">
+                    <button className="btn btn-dark btn-lg" onClick={handleGitHubClick}>
+                        <i className="bi bi-github"></i>
+                    </button>
+                    <button className="btn btn-dark btn-lg" onClick={handleLinkedInClick}>
+                        <i className="bi bi-linkedin"></i>
+                    </button>
+                    <button className="btn btn-dark btn-lg" onClick={handleInstagramClick}>
+                        <i className="bi bi-instagram"></i>
+                    </button>
+                </div>
+            </div>
         </footer>
     );
 }
