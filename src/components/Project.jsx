857d41b5c4094d75d07d523fc5d58fe6aba7da6e
Connor Dailey <connorldailey@gmail.com>
2024-11-07 17:04:41 -0500

    Updates to portfolio page styles

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,17 +1,24 @@
-export default function Project({ title, image, githubLink, deployedLink }) {
+export default function Project({ title, image, description, githubLink, deployedLink }) {
     return (
-        <div className="col-lg-6 mb-4">
-            <div className="card h-100">
-                <div className="card-body">
-                    <h5 className="card-title mb-0">{title}</h5>
-                    <img src={image} alt={title} className="img-fluid my-3"/>
-                    <div className="d-flex justify-content-between">
-                        <a href={deployedLink} target="_blank" rel="noopener noreferrer" className="btn btn-primary">
-                            View App
-                        </a>
-                        <a href={githubLink} target="_blank" rel="noopener noreferrer" className="btn btn-secondary">
-                            GitHub
-                        </a>
+        <div className="col-lg-4 col-md-6 mb-4">
+            <div className="card square-card h-100">
+                <div className="card-body p-0">
+                    <img src={image} alt={title} className="img-fluid"/>
+                    <div className="p-3">
+                        <div className="border-bottom pb-3">
+                            <h3 className="card-title mb-2">{title}</h3>
+                            <p className="mb-0">
+                                {description}
+                            </p>
+                        </div>
+                        <div className="d-flex justify-content-between pt-3">
+                            <a href={deployedLink} target="_blank" rel="noopener noreferrer" className="btn btn-primary w-50 me-3">
+                                View App
+                            </a>
+                            <a href={githubLink} target="_blank" rel="noopener noreferrer" className="btn btn-secondary w-50">
+                                GitHub
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -9,36 +9,42 @@ export default function Portfolio() {
         {
             title: 'Trivial Code',
             image: trivialCodeImg,
+            description: "A spooky game to test your coding knowledge.",
             githubLink: 'https://github.com/Connorldailey/Trivial-Code',
             deployedLink: 'https://connorldailey.github.io/Trivial-Code/',
         },
         {
             title: 'Weather Dashboard',
             image: weatherDashboardImg,
+            description: 'A simple 5 day weather dashboard.',
             githubLink: 'https://github.com/Connorldailey/Weather-Dashboard',
             deployedLink: 'https://module9-challenge-b2vj.onrender.com',
         },
         {
             title: 'Weather Dashboard',
             image: weatherDashboardImg,
+            description: 'A simple 5 day weather dashboard.',
             githubLink: 'https://github.com/Connorldailey/Weather-Dashboard',
             deployedLink: 'https://module9-challenge-b2vj.onrender.com',
         },
         {
             title: 'Weather Dashboard',
             image: weatherDashboardImg,
+            description: 'A simple 5 day weather dashboard.',
             githubLink: 'https://github.com/Connorldailey/Weather-Dashboard',
             deployedLink: 'https://module9-challenge-b2vj.onrender.com',
         },
         {
             title: 'Weather Dashboard',
             image: weatherDashboardImg,
+            description: 'A simple 5 day weather dashboard.',
             githubLink: 'https://github.com/Connorldailey/Weather-Dashboard',
             deployedLink: 'https://module9-challenge-b2vj.onrender.com',
         },
         {
             title: 'Weather Dashboard',
             image: weatherDashboardImg,
+            description: 'A simple 5 day weather dashboard.',
             githubLink: 'https://github.com/Connorldailey/Weather-Dashboard',
             deployedLink: 'https://module9-challenge-b2vj.onrender.com',
         },
@@ -46,7 +52,7 @@ export default function Portfolio() {
 
     return (
         <section>
-            <h2>Portfolio</h2>
+            <h2 className="mb-4">Portfolio</h2>
             <div className="container">
                 <div className="row">
                     {projects.map((project, index) => (
@@ -54,6 +60,7 @@ export default function Portfolio() {
                             key={index}
                             title={project.title}
                             image={project.image}
+                            description={project.description}
                             githubLink={project.githubLink}
                             deployedLink={project.deployedLink}
                         />
