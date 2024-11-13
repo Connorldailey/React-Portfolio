fee3be483433aa9c5bba4a24d0ea160102e1360e
Connor Dailey <connorldailey@gmail.com>
2024-11-13 16:45:23 -0500

    Add comments to Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
 export default function Footer() {
 
+    // Function to handle GitHub button click
     const handleGitHubClick = () => {
         window.open("https://github.com/connorldailey" , "_blank");
     };
 
+    // Function to handle LinkedIn button click
     const handleLinkedInClick = () => {
         window.open("https://www.linkedin.com/in/connor-dailey-7a129b2b2/", "_blank");
     };
 
+    // Function to handle Instagram button click
     const handleInstagramClick = () => {
         window.open("https://www.instagram.com/connorldailey", "_blank");
     };
@@ -16,13 +19,16 @@ export default function Footer() {
         <footer className="p-3">
             <div className="container-fluid">
                 <div className="d-flex justify-content-center">
-                    <button className="btn btn-lg mx-3 footer-icon" onClick={handleGitHubClick}>
+                    {/* GitHub Button */}
+                    <button className="btn btn-lg mx-3 footer-icon" onClick={handleGitHubClick} aria-label="GitHub Profile">
                         <i className="bi bi-github fs-1 text-light"></i>
                     </button>
-                    <button className="btn btn-lg mx-3 footer-icon" onClick={handleLinkedInClick}>
+                    {/* LinkedIn Button */}
+                    <button className="btn btn-lg mx-3 footer-icon" onClick={handleLinkedInClick} aria-label="LinkedIn Profile">
                         <i className="bi bi-linkedin fs-1 text-light"></i>
                     </button>
-                    <button className="btn btn-lg mx-3 footer-icon" onClick={handleInstagramClick}>
+                    {/* Instagram Button */}
+                    <button className="btn btn-lg mx-3 footer-icon" onClick={handleInstagramClick} aria-label="Instagram Profile">
                         <i className="bi bi-instagram fs-1 text-light"></i>
                     </button>
                 </div>
